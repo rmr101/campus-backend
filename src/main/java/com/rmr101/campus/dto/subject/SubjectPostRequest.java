88a6fb2724f80fdf0ec737d16f58d32d99c2f487
HD9990 <vansword126@gmail.com>
2020-07-14 14:35:18 +1000

    not null added

--- src/main/java/com/rmr101/campus/dto/subject/SubjectPostRequest.java
+++ src/main/java/com/rmr101/campus/dto/subject/SubjectPostRequest.java
@@ -2,9 +2,14 @@ package com.rmr101.campus.dto.subject;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class SubjectPostRequest {
+    @NotNull
     private String name;
-    private String introduction;
+    @NotNull
     private String subjectCode;
+    @NotNull
+    private String introduction;
 }
