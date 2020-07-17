d9af1bf430343086e11bf2edfdf52e096357ae06
nkanyang <jessiehan.au@gmail.com>
2020-07-17 20:13:17 +1000

    add properties of assginment into student assignment

--- src/main/java/com/rmr101/campus/mapper/StudentAssignmentMapper.java
+++ src/main/java/com/rmr101/campus/mapper/StudentAssignmentMapper.java
@@ -7,12 +7,23 @@ import com.rmr101.campus.dto.studentAssignment.StudentAssignmentTeacherPutReques
 import com.rmr101.campus.entity.CourseAssignment;
 import com.rmr101.campus.entity.StudentAssignment;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface StudentAssignmentMapper {
     public CourseAssignmentGetResponse courseAssignmentToCourseAssignmentGetResponse(CourseAssignment courseAssignment);
+    @Mappings({
+            @Mapping(target="courseAssignmentId", source="assignment.id"),
+            @Mapping(target="courseName", source="assignment.courseName"),
+            @Mapping(target="publisher", source="assignment.publisher"),
+            @Mapping(target="title", source="assignment.title"),
+            @Mapping(target="content", source="assignment.content"),
+            @Mapping(target="dueDate", source="assignment.dueDate"),
+            @Mapping(target="acceptanceCriteria", source="assignment.acceptanceCriteria"),
+    })
     public StudentAssignmentGetResponse studentAssignmentToStudentAssignmentGetResponse(StudentAssignment assignment);
     public List<StudentAssignmentGetResponse> studentAssignmentToStudentAssignmentGetResponse(List<StudentAssignment> list);
 
