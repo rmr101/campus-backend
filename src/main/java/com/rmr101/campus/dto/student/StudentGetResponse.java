f3a7f4fe49bb512b8a941412e5af0702bdf96cac
HD9990 <vansword126@gmail.com>
2020-07-20 19:25:51 +1000

    added details in student and teacher get response

--- src/main/java/com/rmr101/campus/dto/student/StudentGetResponse.java
+++ src/main/java/com/rmr101/campus/dto/student/StudentGetResponse.java
@@ -1,7 +1,13 @@
 package com.rmr101.campus.dto.student;
 
+import com.rmr101.campus.entity.StudentAssignment;
+import com.rmr101.campus.entity.StudentCourse;
 import lombok.Data;
 
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -9,4 +15,6 @@ public class StudentGetResponse {
     private UUID uuid;
     private String name;
     private String avatar;
+    private String campusId;
+    private boolean isActive;
 }
