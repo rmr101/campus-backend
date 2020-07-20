85aad3ac575035ca0b3d4fe047aba1a29859696e
HD9990 <vansword126@gmail.com>
2020-07-20 19:27:30 +1000

    removed unnecessary docs

--- src/main/java/com/rmr101/campus/dto/student/StudentGetResponse.java
+++ src/main/java/com/rmr101/campus/dto/student/StudentGetResponse.java
@@ -1,13 +1,6 @@
 package com.rmr101.campus.dto.student;
 
-import com.rmr101.campus.entity.StudentAssignment;
-import com.rmr101.campus.entity.StudentCourse;
 import lombok.Data;
-
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Data
